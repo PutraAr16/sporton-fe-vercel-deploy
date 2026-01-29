fd319f753f4a2d55edff604ceddce8c13dfef547
PutraAr16 <2010631170142@student.unsika.ac.id>
2026-01-29 07:25:00 +0700

    update4N

--- app/(landing)/components/home/products.tsx
+++ app/(landing)/components/home/products.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import Button from "../ui/button";
 import { FiPlus } from "react-icons/fi";
 import priceFormatter from "@/app/utils/price-formatter";
-import { Product } from "@/app/type";
+import { Product } from "@/app/types";
 import { getImageUrl } from "@/app/lib/api";
 import { useCartStore } from "@/app/hooks/use-cart-store";
 
@@ -14,24 +14,26 @@ type TProductsProps = {
 };
 
 const ProductsSection = ({ products }: TProductsProps) => {
-  const {addItem} = useCartStore();
+  const { addItem } = useCartStore();
 
-  const handleAddToCart = (e: React.MouseEvent, product: Product) => {
+  const handleAddtoCart = (e: React.MouseEvent, product: Product) => {
     e.preventDefault();
     e.stopPropagation();
     addItem(product);
-  }
+  };
 
   return (
     <section id="products-section" className="container mx-auto mt-32 mb-52">
       <h2 className="font-bold italic text-4xl text-center mb-11">
-        <span className="text-primary">OUR </span> PRODUCTS
+        <span className="text-primary">OUR </span>PRODUCTS
       </h2>
       <div className="grid grid-cols-4 gap-5">
         {products.map((product) => (
-          <Link href={`/product/${product._id}`} 
-                key={product._id} 
-                className="p-1.5 bg-white hover:drop-shadow-xl duration-300">
+          <Link
+            href={`/product/${product._id}`}
+            key={product._id}
+            className="p-1.5 bg-white hover:drop-shadow-xl duration-300"
+          >
             <div className="bg-primary-light aspect-square w-full flex justify-center items-center relative">
               <Image
                 src={getImageUrl(product.imageUrl)}
@@ -40,14 +42,14 @@ const ProductsSection = ({ products }: TProductsProps) => {
                 height={300}
                 className="aspect-square object-contain"
               />
-              <Button className="w-10 h-10 p-2! absolute right-3 top-3" 
-                      onClick={(e) => handleAddToCart(e, product)}>
+              <Button
+                className="w-10 h-10 p-2! absolute top-3 right-3 "
+                onClick={(e) => handleAddtoCart(e, product)}
+              >
                 <FiPlus size={24} />
               </Button>
             </div>
-            <h3 className="font-medium text-lg mb-1.5 mt-4">
-              {product.name}
-            </h3>
+            <h3 className="font-medium text-lg mb-1.5 mt-4">{product.name}</h3>
             <div className="flex justify-between mb-8">
               <div className="text-gray-500">{product.category.name}</div>
               <div className="font-medium text-primary">
--- app/(landing)/components/layouts/header.tsx
+++ app/(landing)/components/layouts/header.tsx
@@ -2,17 +2,17 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import {FiSearch, FiShoppingBag} from "react-icons/fi";
+import { FiSearch, FiShoppingBag } from "react-icons/fi";
 import CartPopup from "../ui/cart-popup";
-import {useState} from "react";
-import {useCartStore} from "@/app/hooks/use-cart-store";
+import { useState } from "react";
+import { useCartStore } from "@/app/hooks/use-cart-store";
 
 const Header = () => {
-  const {items} = useCartStore();
+  const { items } = useCartStore();
   const [isCartPopupOpen, setIsCartPopupOpen] = useState(false);
 
   return (
-    <header className="fixed w-full backdrop-blur-xl bg-white/50">
+    <header className="fixed w-full z-20 backdrop-blur-xl bg-white/50">
       <div className="flex justify-between gap-10 container mx-auto py-7">
         <Link href="/">
           <Image
@@ -22,7 +22,7 @@ const Header = () => {
             height={30}
           />
         </Link>
-        <nav className="flex gap-44 font-medium">
+        <nav className="flex gap-24 font-medium">
           <Link
             href="#"
             className="relative after:content-[''] after:block after:bg-primary after:rounded-full after:h-[3px] after:w-1/2 after:absolute after:left-1/2 after:-translate-x-1/2 after:translate-y-1"
@@ -30,7 +30,7 @@ const Header = () => {
             Home
           </Link>
           <Link href="#">Category</Link>
-          <Link href="#">Explore Product</Link>
+          <Link href="#">Explore Products</Link>
         </nav>
         <div className="relative flex gap-10">
           <FiSearch size={24} />
